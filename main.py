1fe6cfdd764dc3c822259a6621d67861c6bd4797
Kleper <qdymhd112@gmail.com>
2024-02-19 02:56:48 -0800

    build the paddle

--- main.py
+++ main.py
@@ -4,6 +4,31 @@ screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong game")
+screen.tracer(0)
+
+paddle = Turtle("square")
+paddle.color("white")
+paddle.penup()
+paddle.shapesize(1, 6)
+paddle.setheading(90)
+paddle.goto(350, 0)
 
 
+def up():
+    y = paddle.ycor()
+    if y < 240:
+        paddle.sety(y + 20)
+
+
+def down():
+    y = paddle.ycor()
+    if y > -240:
+        paddle.sety(y - 20)
+
+
+screen.listen()
+screen.onkeypress(up, "w")
+screen.onkeypress(down, "s")
+
+screen.update()
 screen.exitonclick()
