23e185d712ef9f40f288f8ef8c70b959bfc3b159
Kleper <qdymhd112@gmail.com>
2024-02-21 10:40:06 -0800

    feat: Make the collision

---
+++ ball.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.x_axis = 0.2
+        self.y_axis = 0.2
+
+    def move(self):
+        x = self.xcor() + self.x_axis
+        y = self.ycor() + self.y_axis
+        self.goto(x, y)
+
+    def bounce_y(self):
+        self.y_axis *= -1
+    def bounce_x(self):
+       self.x_axis *= -1
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.bounce_x()
