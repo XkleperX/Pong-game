e19408851837b7b4a08c837cdf6f999d4e0d3f05
Kleper <qdymhd112@gmail.com>
2024-02-21 11:11:28 -0800

    feat: Insert the the scoreboard class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.bgcolor("black")
@@ -11,7 +12,6 @@ screen.tracer(0)
 right_paddle = Paddle(350, 0)
 left_paddle = Paddle(-350, 0)
 
-
 screen.listen()
 
 screen.onkey(fun=left_paddle.up, key="w")
@@ -20,21 +20,21 @@ screen.onkey(fun=left_paddle.down, key="s")
 screen.onkey(fun=right_paddle.up, key="Up")
 screen.onkey(fun=right_paddle.down, key="Down")
 
-
 ball = Ball()
 
+scoreboard = Scoreboard()
 
 game_is_on = True
 
 while game_is_on:
-    ball.move()
     screen.update()
+    ball.move()
 
-    # collision for the top and bottom side
-    if ball.ycor() > 290 or ball.ycor() < -290:
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
-    # collision for the left and right paddle
+    # Detect collision with paddle
     if (
         ball.distance(right_paddle) < 50
         and ball.xcor() > 320
@@ -43,11 +43,18 @@ while game_is_on:
     ):
         ball.bounce_x()
 
-    # right paddle
+    # Detect R paddle misses
     if ball.xcor() > 380:
+        scoreboard.l_point()
         ball.reset_position()
 
-    # left paddle
+    # Detect L paddle misses:
     if ball.xcor() < -380:
+        scoreboard.r_point()
         ball.reset_position()
+
+    # Check if either player has reached the winning score
+    if scoreboard.l_score == 10 or scoreboard.r_score == 10:
+        game_is_on = False
+
 screen.exitonclick()
